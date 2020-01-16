27581fcb63aeeb52ce10bbbae3aebc833d59a931
askonlinesolutions <info@askonlinesolutions.com>
2020-01-16 12:39:10 +0530

    changes in TEST2 file

--- app/src/main/java/com/example/abhishek/testingpurpose/Firebase/Test2.java
+++ app/src/main/java/com/example/abhishek/testingpurpose/Firebase/Test2.java
@@ -1,4 +1,18 @@
 package com.example.abhishek.testingpurpose.Firebase;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 public class Test2  {
+
+    
+    int a=10;
+
+    void open(){
+        System.out.print(a);
+
+    }
+
 }
