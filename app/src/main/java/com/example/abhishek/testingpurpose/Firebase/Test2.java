8dce60fc5efe741aedd9147ff8f52e07f7120a16
askonlinesolutions <info@askonlinesolutions.com>
2020-01-16 13:09:33 +0530

    changes in TEST2 file again

--- app/src/main/java/com/example/abhishek/testingpurpose/Firebase/Test2.java
+++ app/src/main/java/com/example/abhishek/testingpurpose/Firebase/Test2.java
@@ -5,13 +5,19 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-public class Test2  {
+public class Test2  extends AppCompatActivity{
 
-    
+
     int a=10;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     void open(){
         System.out.print(a);
+
 
     }
 
